372006e9ff5de0a233c1a914d62bed6b3cf25b52
simone <sdiricco.91@gmail.com>
2021-11-22 14:49:04 +0100

    publish to npm

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# board
+# boardjs
 
 Implementation of firmatajs library based on Promise syntax with helpful extra functions. It's based on firmatajs so, can be used the firmata instance directly.
--- index.js
+++ index.js
@@ -1,97 +1,218 @@
 const Firmata = require("firmata");
 const Serialport = require("serialport");
+const EventEmitter = require("events");
 
-let board = undefined;
+const TIMEOUT_EXEC_PROM = 5;
 
-let getDevices = async () => {
-  const spList = await Serialport.list();
-  const usbDevices = spList
-    .map((device) => {
-      return {
-        name: device.manufacturer,
-        port: device.path,
-        device: device,
-      };
-    })
-    .filter((el) => el.name);
-  return usbDevices;
-};
+let wait = (t_ms) => {
+  return new Promise(res => setTimeout(()=> res(true), t_ms))
+}
 
-let connect = async (path) => {
-  return new Promise((res, rej) => {
-    try {
-      board = new Firmata(path);
-      res(true);
-    } catch (e) {
-      rej(e);
-    }
-  });
-};
+class Board extends EventEmitter {
+  constructor() {
+    super();
+    this.firmata = undefined;
 
-let connect2 = async (path) => {
-  return new Promise((res, rej) => {
-    board = new Firmata(
-      new Serialport(
-        path,
-        {
-          autoOpen: false,
-        },
-        (e) => {
-            if (e) {
-                rej(e)
-            }
-            res(true)
-        }
-      )
-    );
-  });
-};
-
-let open = async () => {
-  return new Promise((resolve, reject) => {
-    board.transport.open((e) => {
-      console.log("Open error", e);
-      e ? reject(e) : resolve(true);
-    });
-  });
-};
-
-let readyBoard = async () => {
-  return new Promise((res, rej) => {
-    board.on("ready", () => {
-      res(true);
-    });
-  });
-};
-
-let wait = (t) => {
-  return new Promise((res) => setTimeout(() => res(true), t));
-};
-
-let main = async () => {
-  try {
-    const usbDevices = await getDevices();
-    console.log(usbDevices);
-    const path = usbDevices[0].port;
-    await connect(path)
-    board.on("ready", ()=> {
-        console.log("ready")
-        console.log("disconnect the board, then reconnect")
-    })
-    // await wait(5000);
-    // board.transport.close((e) => {
-    //     console.log("port closed")
-    // })
-    await wait(10000);
-    await connect(path)
-    board.on("ready", ()=> {
-        console.log("ready")
-    })
-    await wait(5000);
-
-  } catch (e) {
-    console.log(e.message);
+    this.__onDisconnect = this.__onDisconnect.bind(this);
+    this.__onClose = this.__onClose.bind(this);
+    this.__onError = this.__onError.bind(this);
+    this.execProm = this.execProm.bind(this);
+    this.requestPort = this.requestPort.bind(this);
+    this.connect = this.connect.bind(this);
+    this.disconnect = this.disconnect.bind(this);
+    this.autoConnect = this.autoConnect.bind(this);
+    this.pinMode = this.pinMode.bind(this);
+    this.digitalWrite = this.digitalWrite.bind(this);
+    this.digitalRead = this.digitalRead.bind(this);
   }
-};
 
-main();
+  get serialport() {
+    if (
+      !(this.firmata instanceof Firmata) &&
+      !(this.firmata.transport instanceof Serialport)
+    ) {
+      return undefined;
+    }
+    return this.firmata.transport;
+  }
+
+  get connected() {
+    if (!(this.firmata instanceof Firmata)) {
+      return false;
+    }
+    return this.firmata.versionReceived && this.firmata.isReady;
+  }
+
+  get port() {
+    return this.serialport.path;
+  }
+
+  get pins() {
+    if (!(this.firmata instanceof Firmata)) {
+      return undefined;
+    }
+    return this.firmata.pins;
+  }
+
+  get MODES() {
+    if (!(this.firmata instanceof Firmata)) {
+      return undefined;
+    }
+    return this.firmata.MODES;
+  }
+
+  get HIGH() {
+    if (!(this.firmata instanceof Firmata)) {
+      return undefined;
+    }
+    return this.firmata.HIGH;
+  }
+
+  get LOW() {
+    if (!(this.firmata instanceof Firmata)) {
+      return undefined;
+    }
+    return this.firmata.LOW;
+  }
+
+  __onDisconnect() {
+    this.firmata = undefined;
+    this.emit("error", {
+      type: "DISCONNECTED",
+      message: `Board disconnected`,
+      details: `details...`,
+    });
+  }
+
+  __onClose() {
+    this.firmata = undefined;
+    this.emit("error", {
+      type: "CLOSE",
+      message: `Board disconnected. Check the hardware configuration`,
+      details: `details...`,
+    });
+  }
+
+  __onError(e) {
+    this.firmata = undefined;
+    this.emit("error", {
+      type: "ERROR",
+      message: `Error`,
+      details: `${e.message}`,
+    });
+  }
+
+  async execProm(__function, timeout = TIMEOUT_EXEC_PROM){
+    return new Promise(async (res, rej) => {
+      setTimeout(() => {
+        rej(new Error('TIMEOUT EXPIRED'));
+      }, timeout);
+      try {
+        __function()
+        this.firmata.flushDigitalPorts(); 
+      } catch (e) {
+        rej(e);
+      }
+      while (this.firmata.pending) {
+        await wait(1);
+      }
+      res(true);
+    });
+  }
+
+  async requestPort() {
+    return new Promise((res, rej) => {
+      Firmata.requestPort((e, port) => {
+        e ? rej(e) : res(port);
+      });
+    });
+  }
+
+  async connect(port, options) {
+    return new Promise((res, rej) => {
+      try {
+        this.firmata = new Firmata(port, options);
+        this.firmata.on("ready", () => {
+          res(true);
+        });
+        this.firmata.on("disconnect", this.__onDisconnect);
+        this.firmata.on("close", this.__onClose);
+        this.firmata.on("error", this.__onError);
+      } catch (e) {
+        this.firmata = undefined;
+        rej("Connection Failed. Check the hardware configuration");
+      }
+    });
+  }
+
+  async disconnect() {
+    return new Promise((res, rej) => {
+      if (!(this.firmata instanceof Firmata)) {
+        res(true);
+      }
+      this.firmata.off("disconnect", this.__onDisconnect);
+      this.firmata.off("close", this.__onClose);
+      this.firmata.off("error", this.__onError);
+      this.firmata.transport.close((e) => {
+        this.firmata = undefined;
+        e ? rej(e) : res(true);
+      });
+    });
+  }
+
+  async reset() {
+    try {
+      await this.execProm(()=>{
+        for (let pin = 0; pin < this.pins.length; pin++) {
+          this.pinMode(pin, this.MODES.UNKOWN);
+          this.digitalWrite(pin, this.LOW, true);
+        }
+      }, 50)
+    } catch (e) {
+      throw new Error(`reset() failed. Details: ${e.message}`)
+    }
+
+  }
+
+  async autoConnect() {
+    try {
+      const port = await this.requestPort();
+      await this.connect(port.path);
+      return true;
+    } catch (e) {
+      throw `autoConnect() failed. ${e.message}`;
+    }
+  }
+
+  async pinMode(pin, mode) {
+    try {
+      await this.execProm(()=>{
+        this.firmata.pinMode(pin, mode);
+      })
+    } catch (e) {
+      throw new Error(`pinMode() failed. Details: ${e}`)
+    }
+  }
+
+  async digitalWrite(pin, value, enqueue) {
+    try {
+      await this.execProm(()=>{
+        this.firmata.digitalWrite(pin, value, enqueue);
+      })
+    } catch (e) {
+      throw new Error(`digitalWrite() failed. Details: ${e.message}`)
+    }
+  }
+
+  async digitalRead(pin) {
+    return new Promise((res, rej) => {
+      try {
+        this.firmata.digitalRead(pin, (value) => res(value));
+      } catch (e) {
+        rej(`digitalRead() failed. Details: ${e.message}`);
+      }
+    });
+  }
+}
+
+module.exports = { Board, Firmata, Serialport };
